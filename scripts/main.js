5acb0632a62cddbdaba58a6f30fc6ef027d7db50
Беличенко Василий Сергеевич <belkavas78@mail.ru>
2025-10-13 15:34:31 +0300

    js delete button

--- scripts/main.js
+++ scripts/main.js
@@ -26,10 +26,12 @@ function renderItem(printContainer, [id, {value}]) {
     }
     item.setAttribute("id", id);
     item.setAttribute("class", "todo-item");
+    item.setAttribute("done", false);
     const text = document.createElement("div");
     const editButton = document.createElement("button");
     const deleteButton = document.createElement("button");
     text.setAttribute("class", "item-text");
+    text.setAttribute("done", false);
     text.textContent = value;
     editButton.setAttribute("class", "item-edit-button");
     editButton.onclick = editItem;
@@ -47,10 +49,15 @@ function renderItem(printContainer, [id, {value}]) {
 function editItem(event) {
     event.preventDefault();
     const item = event.target.parentNode;
+    const oldItem = todoItems.get(parseInt(item.id))
     const newItem = document.createElement("div");
-    newItem.setAttribute("id", item.id);
+    newItem.setAttribute("id", parseInt(item.id));
     newItem.setAttribute("class", "todo-item");
     const input = document.createElement("input");
+    if (oldItem.isDone) {
+        newItem.setAttribute("done", true);
+        input.setAttribute("done", true);
+    }
     const confirmButton = document.createElement("button");
     const cancelButton = document.createElement("button");
     input.setAttribute("class", "edit-input");
@@ -71,11 +78,12 @@ function editItem(event) {
 function confirmEdit(event) {
     event.preventDefault();
     const item = event.target.parentNode;
+    const oldItem = todoItems.get(parseInt(item.id))
     const newItem = document.createElement("div");
     const todoItem = todoItems.get(parseInt(item.id));
     todoItem.value = item.querySelector(".edit-input").value;
-    todoItems.set(item.id, todoItem);
-    newItem.setAttribute("id", item.id);
+    todoItems.set(parseInt(item.id), todoItem);
+    newItem.setAttribute("id", parseInt(item.id));
     newItem.setAttribute("class", "todo-item");
     const text = document.createElement("div");
     const editButton = document.createElement("button");
@@ -87,7 +95,14 @@ function confirmEdit(event) {
     editButton.textContent = "Редактировать";
     deleteButton.setAttribute("class", "item-delete-button");
     deleteButton.onclick = deleteItem;
-    deleteButton.textContent = "Удалить";
+    if (oldItem.isDone) {
+        newItem.setAttribute("done", true);
+        text.setAttribute("done", true);
+        deleteButton.textContent = "Возобновить";
+    }
+    else {
+        deleteButton.textContent = "Удалить";
+    }
     newItem.appendChild(text);
     newItem.appendChild(editButton);
     newItem.appendChild(deleteButton);
@@ -96,9 +111,9 @@ function confirmEdit(event) {
 
 function cancelEdit(event) {
     const item = event.target.parentNode;
-    const oldItem = todoItems.get(parseInt(event.target.parentNode.id));
+    const oldItem = todoItems.get(parseInt(item.id));
     const newItem = document.createElement("div");
-    newItem.setAttribute("id", event.target.parentNode.id);
+    newItem.setAttribute("id", parseInt(item.id));
     newItem.setAttribute("class", "todo-item");
     const text = document.createElement("div");
     const editButton = document.createElement("button");
@@ -110,7 +125,14 @@ function cancelEdit(event) {
     editButton.textContent = "Редактировать";
     deleteButton.setAttribute("class", "item-delete-button");
     deleteButton.onclick = deleteItem;
-    deleteButton.textContent = "Удалить";
+    if (oldItem.isDone) {
+        newItem.setAttribute("done", true);
+        text.setAttribute("done", true);
+        deleteButton.textContent = "Возобновить";
+    }
+    else {
+        deleteButton.textContent = "Удалить";
+    }
     newItem.appendChild(text);
     newItem.appendChild(editButton);
     newItem.appendChild(deleteButton);
@@ -119,7 +141,23 @@ function cancelEdit(event) {
 
 
 function deleteItem(event) {
-
+    const item = event.target.parentNode;
+    const oldItem = todoItems.get(parseInt(item.id));
+    if (oldItem.isDone) {
+        item.setAttribute("done", false);
+        item.querySelector(".item-delete-button").textContent = "Удалить";
+        item.querySelector(".item-text").setAttribute("done", false);
+        oldItem.isDone = false;
+        todoItems.set(parseInt(item.id), oldItem);
+    }
+    else {
+        item.setAttribute("done", true);
+        item.querySelector(".item-delete-button").textContent = "Возобновить";
+        item.querySelector(".item-text").setAttribute("done", true);
+        oldItem.isDone = true;
+        todoItems.set(parseInt(item.id), oldItem);
+    }
+    console.log(todoItems)
 }
 
 
